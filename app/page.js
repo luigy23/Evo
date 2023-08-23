3d4387efecccbea965ff538fcf7ece4d5df6989f
luigy23 <luigyleonardo23@gmail.com>
2023-08-23 18:20:25 -0500

    primera version evo

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-smoke-900">
+
+      <h1>
+        Hola
+      </h1>
+     
+    </main>
+  )
+}
