fd962b8a4dae663c686c48e096d9a06e478faf03
luigy23 <luigyleonardo23@gmail.com>
2023-08-24 22:41:48 -0500

    botones

--- app/page.js
+++ app/page.js
@@ -1,15 +1,35 @@
 "use client"
 
-import { Button } from '@nextui-org/react'
+import { Button, Card, CardBody, CardFooter, Input, Textarea } from '@nextui-org/react'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-smoke-900">
+  const [value, setValue] = useState('')
 
-      <h1>
-        Hola
-      </h1>
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-smoke-900 gap-3 dark">
+
+    <Card shadow='md' width='300px' height='300px' className='flex flex-col items-center justify-center gap-3'
+    isBlurred={true} 
+    >
+      <CardBody>
+      <h1>¿Qué quieres hacer?</h1>
+      </CardBody>
+      <CardFooter className='gap-2 justify-center' >
+      <Button variant='shadow' color='success'>📅 Rutina</Button>
+      <Button variant='shadow' color='primary'>📝 Nota</Button>
+      <Button variant='shadow' color='default'>🎯 Objetivo</Button>
+      <Button variant='shadow' color='warning'>📈 Ventas</Button>
+      </CardFooter>
+    
+    </Card>
+
+      <Textarea placeholder='Type something' label='Hola'  color='primary' minRows={2}
+      value={value} onChange={(e) => setValue(e.target.value)}
+      />
+
       <Button variant='shadow' color='success'>Click me</Button>
      
     </main>
